548a5a1ef270475270bbc1342e38f66439e053c7
lmarkus <lmarkus@paypal.com>
2013-12-03 16:41:22 -0800

    Configure database during the app configuration stage

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 
 var kraken = require('kraken-js'),
+    db = require('./lib/database'),
     app = {};
 
 
 app.configure = function configure(nconf, next) {
     // Fired when an app configures itself
+    //Configure the database
+    db.config(nconf.get('databaseConfig'));
     next(null);
 };
 
