479a9747443806ac13fa16e9c02eef9c83754135
lmarkus <lmarkus@paypal.com>
2013-12-05 11:22:34 -0800

    Modified index controller and template to present a list of products

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,27 @@
 'use strict';
-
+var Product = require('../models/productModel');
 
 module.exports = function (server) {
 
+    /**
+     * Display a list of the products.
+     */
     server.get('/', function (req, res) {
-        var model = { name: 'kraken-example-shopping-cart' };
-        
-        res.render('index', model);
-        
+
+        Product.find(function (err, prods) {
+            if (err) {
+                console.log(err);
+            }
+
+            var model =
+            {
+                products: prods
+            };
+
+            res.render('index', model);
+
+        });
+
     });
 
 };
